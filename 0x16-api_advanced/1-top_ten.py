92ee5af58afc48884bb6de7da370931b6c8be1be
Salma1009 <salma.m1009@yahoo.com>
2023-06-21 00:33:11 -0700

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Print hot posts on a given Reddit subreddit."""
+"""Function to print hot posts on a given Reddit subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Return the titles of the 10 hottest posts."""
+    """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "limit": 10
